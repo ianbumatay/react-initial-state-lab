3c7e252bcf40af645431be5b6cc65a16860d814c
IAN BUMATAY <IANBUMATAY.09@GMAIL.COM>
2021-05-17 16:32:09 -0400

    commit tree 3cfeac1548c909c9504a73f3136c8a36cb927a78

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,31 @@
-// your Bomb code here!
+// your Bomb code here! 
+
+import React, {Component} from 'react' 
+
+export class Bomb extends Component { 
+
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    render(){
+        console.log(this.props.initialCount)
+        console.log(this.state.secondsLeft)
+
+        return(
+            <div>
+                {this.state.secondsLeft === 0 ? 'Boom!' : this.state.secondsLeft  + ' seconds left before I go boom!' }
+            </div>
+        )
+    }
+
+
+
+} 
+
+export default Bomb
